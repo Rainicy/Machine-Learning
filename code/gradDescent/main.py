dcd528eb322acf41a23a7b69b883f431d96598ff
Rainicy <rainicy925@gmail.com>
2014-05-27 23:06:07 -0400

    Change the BGD to linear BGD, and seprated the code

---
+++ code/gradDescent/main.py
@@ -0,0 +1,38 @@
+'''
+Created May 27, 2014
+
+@author Rainicy
+'''
+
+import numpy as np
+
+from LinearBatchGD import LinearBatchGD
+from util import RMSE, initialData
+
+def main():
+
+	# For testing
+	np.set_printoptions(threshold='nan')
+
+	# Part 1: Prepare for the data for training and testing
+
+	data = np.loadtxt('../../data/spambase/spambase.data', delimiter=',')
+	trainX, trainY, testX, testY = initialData(data)
+
+	# Part 2: Training the theta model by Batch Gradient Descent
+	alpha = 5e-5
+	threshold = 1e-6
+	theta = LinearBatchGD(trainX, trainY, alpha, threshold)
+
+	# Part 3: Testing data 
+	predictTrain = np.dot(trainX, theta)
+	predictTest = np.dot(testX, theta)
+
+	rmseTrain = RMSE(predictTrain, trainY)
+	rmseTest = RMSE(predictTest, testY)
+
+	print "RMSE for Training Data: %f" % rmseTrain
+	print "RMSE for Testing Data: %f " % rmseTest
+
+if __name__ == '__main__':
+	main()
--- code/gradDescent/util.py
+++ code/gradDescent/util.py
@@ -1,14 +1,11 @@
 '''
-Created on May 27, 2014
+Created May 27, 2014
 
 @author Rainicy
 '''
 
 import numpy as np
-import pandas as pd
 
-
-		
 def initialData(data):
 	'''
 	Description: This function split the datta to training and testing set and 
@@ -50,7 +47,6 @@ def initialData(data):
 	testX = np.insert(testX, 0, 1, axis = 1)
 	return trainX, trainY, testX, testY
 
-
 def RMSE(h, y):
 		'''
 		Description: Root Mean Squared Error(RMSE). J = sum(h - y)^2
@@ -68,71 +64,3 @@ def RMSE(h, y):
 		return np.sqrt(J/y.size)
 		
 
-def BatchGD(X, y, alpha=5e-5, threshold=1e-3):
-	'''
-	Description: This algorithms represents the Batch Gradient Descent algorithm.
-
-	@param:
-		X: training features
-		y: training labels
-		alpha: learning rate
-		threshold: the threshold for terminate the loop
-	@return:
-		theta: the parameters, which have been trained for feture testing
-	'''
-
-	# m: #samples, n:#features
-	m, n = np.shape(X)
-	# intialize the theta with all 0s
-	theta = np.zeros(n)
-	loop = 0
-
-	# initialize the RMSE for terminating the loop
-	hypothese = np.dot(X, theta)
-	rmse = RMSE(hypothese, y)
-	rmse_ = np.inf
-	xTrans = np.transpose(X)
-	while abs(rmse - rmse_) > threshold:
-		loop += 1
-		if loop == 1:
-			rmse_ = np.inf
-		else:
-			rmse_ = rmse
-		hypothese = np.dot(X, theta)
-		rmse = RMSE(hypothese, y)
-		print 'Iteration: %d | RMSE: %f' % (loop, rmse)
-
-		# updating parameters
-		theta = theta + alpha * np.dot(xTrans, (y - hypothese))
-
-	return theta
-
-
-
-def main():
-
-	# For testing
-	np.set_printoptions(threshold='nan')
-
-	# Part 1: Prepare for the data for training and testing
-
-	data = np.loadtxt('../data/spambase/spambase.data', delimiter=',')
-	trainX, trainY, testX, testY = initialData(data)
-
-	# Part 2: Training the theta model by Batch Gradient Descent
-	alpha = 5e-5
-	threshold = 1e-6
-	theta = BatchGD(trainX, trainY, alpha, threshold)
-
-	# Part 3: Testing data 
-	predictTrain = np.dot(trainX, theta)
-	predictTest = np.dot(testX, theta)
-
-	rmseTrain = RMSE(predictTrain, trainY)
-	rmseTest = RMSE(predictTest, testY)
-
-	print "RMSE for Training Data: %f", rmseTrain
-	print "RMSE for Testing Data: %f", rmseTest
-
-if __name__ == '__main__':
-	main()
