5fb50f1e356b6ddcdff1dec11be4a0135a41d7ac
Rainicy <rainicy925@gmail.com>
2014-06-10 09:56:04 -0400

    Add outter loop for SVM

--- code/SVM/SVM.py
+++ code/SVM/SVM.py
@@ -7,8 +7,19 @@ Created on June 9, 2014
 '''
 
 from numpy import *
+import time
 
 def calKernel(X, x, kernel):
+	'''
+	Description: Calculate the Kernel(K[:, i]) by one column. 
+
+	@param:
+		X: X[m, n] entire training features
+		x: x[1, n] one sample data, let's say index is i
+		kernel: the kernel options {linear | rbf}
+	@return:
+		K[m, i]: update one column Kernel by given sample data.(Kmi)
+	'''
 	m = shape(X)[0]
 	K = mat(zeros((m, 1)))
 	if kernel[0] == 'linear':	# linear kernel
@@ -27,7 +38,7 @@ def calKernel(X, x, kernel):
 
 
 
-class optStruct:
+class SVMStruct:
 	'''
 	Description: class for saving SVM model data
 	'''
@@ -43,6 +54,40 @@ class optStruct:
 		self.K = mat(zeros((self.m, self.m)))
 		for i in range(self.m):	# initialize the kernel matrix
 			self.K[:, i] = calKernel(self.X, self.x[i, :], kernel)
+
+
+def calEk(svm, k):
+	'''
+	Description: Calculate the error for given x(k)
+				 Formula: E(k) = f(x(k)) - y(k)
+				 		  f(x(k)) = sum(y(i)*alphas(i)*K(i,k)) + b
+
+	@param:
+		svm: the SVM struct
+		k: the error calculated for sample index k
+	@return:
+		error: f(x(k)) - y(k) [hypothese(k) - y(k)]
+	'''
+	hypothese = float(multiply(svm.y, svm.alphas).T * svm.K[:, k] + svm.b)
+	error = hypothese - float(svm.y[k])
+	return error
+
+
+def innerLoop(svm, i):
+	'''
+	Description: Inner loop to update alpha_i and alpha_j.
+
+	@param:
+		svm: the SVM struct
+		i: the alpha_i index
+
+	@return 
+		1: if find alpha_i, which violates KKT conditions, and update alpha_i and alpha_j,
+		   otherwise 0.
+
+	'''
+	# calculate the Error: Ek = f(x(k))- y(k)
+	calEk(svm, i)
 
 
 
@@ -63,10 +108,40 @@ def train(X, y, C, toler, maxIter, kernel):
 		b: refer to notes
 	'''
 
-	svm = optStruct(mat(X), mat(y).T, C, toler, kernel)
+	startTime = time.time()
 
-	# Training loop until:
-	# 1) Maxmium iteration
+	svm = SVMStruct(mat(X), mat(y).T, C, toler, kernel)
+
+	# Termination conditions:
+	# 1) Maxmium iteration or 
 	# 2) No more alpha need to be changed after go through all the data.
 	# 	 which means, all the samples satisfy the KKT conditions.
 	iteration = 0
+	entireSet = True
+	alphaPairChanged = 0
+	while (iteration < maxIter) and ((alphaPairChanged > 0) or (entireSet)):
+		alphaPairChanged = 0
+
+		# update alphas through whole data set
+		if entireSet:
+			for i in range(svm.m):
+				alphaPairChanged += innerLoop(svm, i)
+			print "iter: %d on Entire Set | Alphas Pairs Changed: %d" % (iteration, alphaPairChanged)
+		# update alphas through all non-boundary data set
+		else:
+			nonBoundIndexes = nonzero((svm.alphas > 0) & (svm.alphas < C))[0] # [0] row index
+			for i in nonBoundIndexes:
+				alphaPairChanged += innerLoop(svm, i)
+			print "iter: %d on Non-Bound Set | Alphas Pairs Changed: %d" % (iteration, alphaPairChanged)
+		iteration += 1
+
+		# change update order: Entire Set <=> Non-boundary Set
+		if entireSet:
+			entireSet = False
+		elif alphaPairChanged == 0:
+			entireSet = True
+
+	# training end
+	print 'SVM training costs %fs' % (time.time() - startTime)
+
+	return svm.alphas, svm.b
