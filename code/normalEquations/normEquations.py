1cf9f487c2a7fd773b1f3b0d157b47dac746c4d1
Rainicy <rainicy925@gmail.com>
2014-06-16 16:22:55 -0400

    Add regularized normal equations

--- code/normalEquations/normEquations.py
+++ code/normalEquations/normEquations.py
@@ -4,11 +4,9 @@ Created on May 29, 2014
 @author Rainicy
 '''
 
-from numpy.linalg import inv
-from numpy import transpose
-from numpy import dot
+from numpy import *
 
-def normEquations(X, y):
+def normEquations(X, y, options):
 	'''
 	Description: Use Normal Equations Linear Regression to calculate the theta. 
 				 theta = (X.T * X).Inverse * X.T * y
@@ -16,17 +14,32 @@ def normEquations(X, y):
 	@param:
 		X: training features
 		y: training labels
+		options:	1) regularized: True regularization 
+					2) lambda: regularization parameter
 	@return:
 		theta: the parameters
 	'''
 
-	Xtrans = transpose(X)
+	m, n = shape(X)
+	X = mat(X)
+	y = mat(y).T
+
 	# X.T * X
-	XTX = dot(Xtrans, X)
+	XTX = X.T * X
 
-	# theta
-	theta = dot(dot(inv(XTX), Xtrans), y)
+	# update theta
+	### 1) Nomal:
+	#####	theta = (X.T * X).Inv * X.T * y
+	###	2) Regilarization:
+	#####	theta = (X.T*X + lambda*eye[]
+	if not options['regularized']:
+		theta = (XTX.I * X.T) * y
+	else:
+		diag = eye(n, n)
+		diag[0, 0] = 0
+		theta = (XTX + options['lambda']*diag).I * X.T * y
 
+	# print options['lambda']
 	return theta
 
 
